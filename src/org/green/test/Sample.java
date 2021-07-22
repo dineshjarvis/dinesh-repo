6d435a974c084217b1dadb2863247220386eae17
dineshjarvis <dinesh20sai96@gmail.com>
2021-07-22 13:32:15 +0530

    rani push

--- src/org/green/test/Sample.java
+++ src/org/green/test/Sample.java
@@ -1,10 +1,14 @@
 package org.green.test;
 
+import javax.xml.bind.helpers.DefaultValidationEventHandler;
 public class Sample {
-	
 	public static void simple()
 	{
 		System.out.println("hello world");
+	}
+	public static void deva() {
+		// TODO Auto-generated method stub
+System.out.println("kjgjg");
 	}
   private void username() {
 	// TODO Auto-generated method stub
