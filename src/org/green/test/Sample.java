fa162c7020f7d72ca1ccf8ba9d1ea7fdcd1629f7
dineshjarvis <dinesh20sai96@gmail.com>
2021-07-22 12:39:29 +0530

    sugan push

--- src/org/green/test/Sample.java
+++ src/org/green/test/Sample.java
@@ -1,6 +1,11 @@
 package org.green.test;
 
 public class Sample {
+	
+	public static void password()
+	{
+		System.out.println("sugan");
+	}
   private void username() {
 	// TODO Auto-generated method stub
 System.out.println("username");
